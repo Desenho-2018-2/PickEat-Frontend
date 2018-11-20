f744ae32b9fe6ee15491bb4c06ecc8792a5b9abd
Matheus Costa <matheus.bionicle@gmail.com>
2018-11-20 19:06:40 -0200

    Add templates

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gz6v891$df5my)sd8@+m^be=7q11(xf!gx7um3j$+!bd(5&)#4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = []
 
 
 # Application definition
