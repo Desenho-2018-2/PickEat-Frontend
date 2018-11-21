3720b8582b62f1d44211e7c76e6fd2961f79cf8f
Matheus Costa <matheus.bionicle@gmail.com>
2018-11-21 18:41:26 -0200

    Add index page, add material design and fix settings

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gz6v891$df5my)sd8@+m^be=7q11(xf!gx7um3j$+!bd(5&)#4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dev',
 ]
 
 MIDDLEWARE = [
