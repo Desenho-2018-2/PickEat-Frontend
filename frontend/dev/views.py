0ba4f58fa4a21af8b079b2df88fed464d2b8c1c0
Matheus Costa <matheus.bionicle@gmail.com>
2018-11-21 21:19:36 -0200

    Add tables page

--- frontend/dev/views.py
+++ frontend/dev/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'dev/index.html', {})
+def tables(request):
+    table = [1,2,3,4,5]
+    return render(request, 'dev/tables.html', {"tables":table})
