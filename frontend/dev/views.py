4a4b4fa8b23b12fb2ea82d01ee6c081f018043e2
Cleber Junior <cjj.castro3@gmail.com>
2018-11-22 01:10:17 -0200

    Update initial page, set dynamic tables

--- frontend/dev/views.py
+++ frontend/dev/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'dev/index.html', {})
+    orders = [{'table': 2, 'id': 321, 'status': 'done'}, {'table': 3, 'id': 322, 'status': 'cook'}]
+    return render(request, 'dev/index.html', {'orders': orders})
+
 def tables(request):
     table = [1,2,3,4,5]
     return render(request, 'dev/tables.html', {"tables":table})
